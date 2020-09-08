8113fef4afcae27fd72a042fe47ac644ccd29521
nataliaasaa <nataliaasaa@gmail.com>
2020-09-08 17:07:54 -0300

    Filtro numero de ocorrências por estado

--- withpandas.py
+++ withpandas.py
@@ -4,11 +4,19 @@ class PandasFiltros:
     
     def __init__(self):
         self.path = input("Digite o caminho do arquivo: ")
-        self.file = pd.read_csv(self.path)
+        self.file = pd.read_csv(self.path, sep=";")
 
-    def teste(self):
-        sigla = input("Digite a sigla do estado: ")
-        print (self.file[['Estado/Provincia']])
+    def NumIndiv(self):  #Filtra o numero de indivíduos no estado indicado
+        sigla = input("Digite a sigla do estado: ").upper()
+        numero = self.file.loc[self.file['Estado/Provincia'] == sigla, 'Numero de individuos'].sum()
+        print ("Número de indivíduos em", sigla, ":", numero)
 
+    def NumOccor(self):  #Filtra o numero de ocorrências no estado indicado
+        sigla = input("Digite a sigla do estado: ").upper()
+        count = self.file[self.file['Estado/Provincia'] == sigla]
+        print ("Número de ocorrências em", sigla, ":", len(count))
+        
+        
 obj = PandasFiltros()
-obj.teste()
+obj.NumIndiv()
+obj.NumOccor()
